42e8580101f5094ea4a96acdff9940166dbec951
Li <li@silica.codes>
2026-02-09 11:23:16 +1300

    Fallback on dumping partition if its corrupted or missing?

--- extractgc.c
+++ extractgc.c
@@ -15,24 +15,31 @@ static char scratch[0x1028] = { 0 };
 static uint8_t sector[SECTOR_SIZE] = { 0 };
 static uint8_t buffer[SECTOR_SIZE * 0x5000] = { 0 };
 
-void dump_partiton(FILE* img, ScePartition* part, uint64_t mbr_start_pos, const char* output_file) {
+int dump_partiton(FILE* img, ScePartition* part, uint64_t mbr_start_pos, const char* output_file) {
+
 	FILE* partition = fopen(output_file, "wb");
-	uint64_t total = 0;
+	if (partition != NULL) {
+		uint64_t total = 0;
 
-	uint64_t real_part_offset = mbr_start_pos + (part->offset * SECTOR_SIZE);
-	uint64_t real_part_size = (part->size * SECTOR_SIZE);
+		uint64_t real_part_offset = mbr_start_pos + (part->offset * SECTOR_SIZE);
+		uint64_t real_part_size = (part->size * SECTOR_SIZE);
 
-	fseek(img, real_part_offset, SEEK_SET);
+		fseek(img, real_part_offset, SEEK_SET);
 
-	do {
-		size_t rd = fread(buffer, 1, (size_t)(((total + sizeof(buffer)) <= real_part_size) ? sizeof(buffer) : real_part_size - total), img);
-		total += fwrite(buffer, 1, rd, partition);
-		printf("%s: %llu / %llu (%.0f%%)\r", output_file, total, real_part_size, (((double)total / (double)real_part_size) * 100.0));
-	} while (total < real_part_size);
+		do {
+			size_t rd = fread(buffer, 1, (size_t)(((total + sizeof(buffer)) <= real_part_size) ? sizeof(buffer) : real_part_size - total), img);
+			total += fwrite(buffer, 1, rd, partition);
+			printf("%s: %llu / %llu (%.0f%%)\r", output_file, total, real_part_size, (((double)total / (double)real_part_size) * 100.0));
+		} while (total < real_part_size);
 
-	fclose(partition);
-	printf("\n");
+		fclose(partition);
+		printf("\n");
+		return 1;
+	}
+
+	return 0;
 }
+
 
 
 int main(int argc, char** argv) {
@@ -41,7 +48,7 @@ int main(int argc, char** argv) {
 
 	FATFS fs = { 0 };
 	char gc_img[0x500] = { 0 };
-	char out_folder[0x500] = { 0 };
+	char output_folder[0x500] = { 0 };
 
 	if (argc >= 2) {
 		strncpy(gc_img, argv[1], sizeof(gc_img) - 1);
@@ -59,10 +66,10 @@ int main(int argc, char** argv) {
 	}
 
 	if (argc >= 4) {
-		strncpy(out_folder, argv[3], sizeof(out_folder) - 1);
+		strncpy(output_folder, argv[3], sizeof(output_folder) - 1);
 	}
 	else {
-		change_extension("", gc_img, sizeof(gc_img), out_folder);
+		change_extension("", gc_img, sizeof(gc_img), output_folder);
 	}
 
 	if (file_exists(gc_img)) {
@@ -78,7 +85,7 @@ int main(int argc, char** argv) {
 			printf("Detected: vita cartridge image (VCI)\n");
 			
 			// write keys
-			snprintf(scratch, sizeof(scratch) - 1, "%s/gc_cmd56_keys.bin", out_folder);
+			snprintf(scratch, sizeof(scratch) - 1, "%s/gc_cmd56_keys.bin", output_folder);
 			write_file(scratch, &vci->keys, sizeof(GcCmd56Keys));
 
 			// seek to MBR
@@ -88,11 +95,11 @@ int main(int argc, char** argv) {
 			printf("Detected: psvgamesd (PSV)\n");
 			
 			// write cart secret
-			snprintf(scratch, sizeof(scratch) - 1, "%s/cart-secret.bin", out_folder);
+			snprintf(scratch, sizeof(scratch) - 1, "%s/cart-secret.bin", output_folder);
 			write_file(scratch, psv->cart_secret, sizeof(psv->cart_secret));
 
 			// write keys
-			snprintf(scratch, sizeof(scratch) - 1, "%s/cart-hash.bin", out_folder);
+			snprintf(scratch, sizeof(scratch) - 1, "%s/cart-hash.bin", output_folder);
 			write_file(scratch, psv->cart_hash, sizeof(psv->cart_hash));
 
 			// seek to MBR
@@ -111,6 +118,8 @@ int main(int argc, char** argv) {
 		fread(sector, 1, sizeof(sector), img);
 
 		if (memcmp(mbr->magic, SCE_MBR_MAGIC, sizeof(mbr->magic)) == 0) {
+			create_directories(output_folder, 0);
+
 			for (size_t i = 0; i < (sizeof(mbr->partitions) / sizeof(ScePartition)); i++) {
 				if (mbr->partitions[i].code == ScePartitionCode_EMPTY) continue;			
 				const char* partition_name = partition_code_to_name(mbr->partitions[i].code);
@@ -122,7 +131,7 @@ int main(int argc, char** argv) {
 					uint64_t size = mbr->partitions[i].size * SECTOR_SIZE;
 
 					// get output filename
-					snprintf(scratch, sizeof(scratch) - 1, "%s/%s%s", out_folder, partition_name, modifiers);
+					snprintf(scratch, sizeof(scratch) - 1, "%s/%s%s", output_folder, partition_name, modifiers);
 
 					// extract exfat image
 					ff_init(img, offset, size);
@@ -132,45 +141,49 @@ int main(int argc, char** argv) {
 							res = 0;
 						}
 						else {
-							fprintf(stderr, "failed to read one or more files in the %s: exFAT partition.\n", partition_name);
+							fprintf(stderr, "warn: failed to read one or more files in the %s: exFAT partition.\n", partition_name);
 							break;
 						}
 
 						f_unmount("0:");
 					}
 					else {
-						fprintf(stderr, "%s: file contains an invalid exFAT partition!!!\n", partition_name);
-						break;
+						fprintf(stderr, "warn: expected %s: to contain a exFAT partition; but it is missing or corrupted..\n", partition_name);
+						goto dump;
 					}
 				}
 				else {
+dump:
 					// get partition filename 
-					snprintf(scratch, sizeof(scratch) - 1, "%s/%s%s-%s.img", out_folder, partition_name, modifiers, format_name);
+					snprintf(scratch, sizeof(scratch) - 1, "%s/%s%s-%s.img", output_folder, partition_name, modifiers, format_name);
 
 					// dump partition ..
-					dump_partiton(img, &mbr->partitions[i], mbr_start_pos, scratch);
+					if (!dump_partiton(img, &mbr->partitions[i], mbr_start_pos, scratch)) {
+						fprintf(stderr, "err: failed to dump partition: %s\n", partition_name);
+						break;
+					}
 				}
 
 			}
 		}
 		else {
 			uint64_t size = get_filesize(img);
-			change_extension("", gc_img, sizeof(gc_img), out_folder);
+			change_extension("", gc_img, sizeof(gc_img), output_folder);
 
 			ff_init(img, 0, size);
 
 			if (f_mount(&fs, "0:", 1) == FR_OK) {
-				if (read_recursive("/", out_folder)) {
+				if (read_recursive("/", output_folder)) {
 					res = 0;
 				}
 				else {
-					fprintf(stderr, "failed to read one or more files from an exFAT partition.\n");
+					fprintf(stderr, "err: failed to read one or more files from exFAT partition.\n");
 				}
 
 				f_unmount("0:");
 			}
 			else {
-				fprintf(stderr, "%s: could not find a valid SceMBR, but also the file is not a valid exFAT partition\n", gc_img);
+				fprintf(stderr, "err: %s: could not find a valid SceMBR, but also the file is not a valid exFAT partition\n", gc_img);
 			}
 
 		}
@@ -180,7 +193,7 @@ int main(int argc, char** argv) {
 		return res;
 	}
 	else {
-		fprintf(stderr, "%s: file not found.\n", gc_img);
+		fprintf(stderr, "err: %s: file not found.\n", gc_img);
 	}
 	return res;
 
