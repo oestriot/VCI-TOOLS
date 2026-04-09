761438e1a3624793978b5af2b1009ca3e9b7c9ed
Li <li@noreply.silica.codes>
2026-04-09 23:03:14 +0000

    Update lib/path.c

--- lib/path.c
+++ lib/path.c
@@ -19,12 +19,12 @@ void create_directories(const char* dir_name, uint8_t including_last) {
 	for (size_t i = 0; i < strlen(dir_name); i++) {
 		if (dir_name[i] == '/') {
 			strncpy(last_dir, dir_name, i);
-			mkdir(last_dir, 744);
+			mkdir(last_dir, 0744);
 		}
 	}
 
 	if (including_last) {
-		mkdir(dir_name, 744);
+		mkdir(dir_name, 0744);
 	}
 }
 
